ba6766091e02f6ce7d529bd0ea7ca98e9ceaae5b
Blingblinggiirl <5075@holbertonschool.com>
2022-06-06 19:58:55 -0700

    4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#incllude <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+
+	put ("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
