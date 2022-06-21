fd57b5b3e042c10bb182980fe1a67d60ae618307
Blingblinggiirl <5075@holbertonschool.com>
2022-06-21 13:16:50 -0700

    prints the number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - argc
+ * @argc: counter
+ * @argv: argv
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
