adacf554ec3fecbbfb1622755d36892bee573a97
Blingblinggiirl <5075@holbertonschool.com>
2022-06-21 12:53:17 -0700

    program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name
+ * @argc: counter
+ * @argv: argv
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
