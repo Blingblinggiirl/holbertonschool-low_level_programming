38e73938e0de339eea3ccf691efd0d8b1f0958de
Blingblinggiirl <5075@holbertonschool.com>
2022-06-24 09:35:15 -0700

    malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	rom = malloc((sizeof(char) * size) + 1);
+	rom = malloc((sizeof(char) * size);
 
 	if (rom == NULL)
 		return (NULL);
