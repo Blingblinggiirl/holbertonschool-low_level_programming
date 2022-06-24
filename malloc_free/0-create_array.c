c6e246fd0584f963081a19bbb64d665405e76370
Blingblinggiirl <5075@holbertonschool.com>
2022-06-24 09:32:21 -0700

    malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array
+ * @size: size
+ * @c: char
+ * Return: array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *rom;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	rom = malloc((sizeof(char) * size) + 1);
+
+	if (rom == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		rom[i] = c;
+	return (rom);
+}
+
