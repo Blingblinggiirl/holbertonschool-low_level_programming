3ef3c8fe683cb69b3b7f56f276aefed59a47194e
Blingblinggiirl <5075@holbertonschool.com>
2022-06-26 15:32:38 -0700

    1

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,27 +11,27 @@ char *str_concat(char *s1, char *s2)
 {
 	int i = 0, j = 0;
 	int sum;
-	char *buffer = NULL;
+	char *buffer;
 
-	if (!s1 && !s2)
-		return (NULL);
-	if (!s1 && s2)
-		sum = strlen(s2);
-	if (s1 && !s2)
+	if (s1 != NULL && s2 == NULL)
 		sum = strlen(s1);
+	if (s1 == NULL && s2 != NULL)
+		sum = strlen(s2);
 	if (s1 != NULL && s2 != NULL)
-		sum = strlen(s1) + strlen(s2);
-	sum = sum + 1;
+		sum = (strlen(s1) + strlen(s2));
+	sum += 1;
 	buffer = malloc(sum);
 	if (buffer == NULL)
 		return (NULL);
-	if (!s1)
+	if (s1 != NULL)
+	{
 		for (i = 0; s1[i]; i++)
 		{
 			buffer[j] = s1[i];
 			j++;
 		}
-	if (!s2)
+	}
+	if (s2 != NULL)
 	{
 		for (i = 0; s2[i]; i++)
 		{
