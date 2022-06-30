4c062af4318e22b8367191145b39b9702052de5f
Blingblinggiirl <5075@holbertonschool.com>
2022-06-29 20:05:13 -0700

    structure

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+This repository has concepts and tasks about structures and how to use them. It aldo contains typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure
+ * @name: name of the dog
+ * @age: age of the god
+ * @owner: owner of the god
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
