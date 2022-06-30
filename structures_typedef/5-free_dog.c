f827301ffb21eae00d2ffc3330e51e6de5733795
Blingblinggiirl <5075@holbertonschool.com>
2022-06-30 06:56:04 -0700

    free memory allocation

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * fre_dog - free memoru allocation of the structure dog
+ * free_dog - free memoru allocation of the structure dog
  * @d: dog
  */
 void free_dog(dog_t *d)
