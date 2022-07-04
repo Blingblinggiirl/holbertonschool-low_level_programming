1392b18d11ff44d29440cfb17ac16a2a07986906
Blingblinggiirl <5075@holbertonschool.com>
2022-07-04 14:00:30 -0700

    Prints strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - print strings
+ * @n: n
+ * @separator: printed between strings
+ * Return: ap
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *pp;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		pp = va_arg(ap, char*);
+		printf("%s", pp ? pp : "(nill)");
+		if (i != n - 1 && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
