23074386492b68123e28d00934a3cedd1a092560
Blingblinggiirl <5075@holbertonschool.com>
2022-07-13 17:49:43 -0700

    Prints all the elements of a list_t list.

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 int _putchar(char c);
 /**
  * struct list_s - singly linked list
@@ -12,9 +12,9 @@ int _putchar(char c);
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-}list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
 #endif
