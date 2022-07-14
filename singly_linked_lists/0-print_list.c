4f7f20f7364cb4a771d00c6f8c1a3a59cf9b510c
Blingblinggiirl <5075@holbertonschool.com>
2022-07-13 17:45:42 -0700

    Prints all the elements of a list_t list.

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,16 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *print_list - print elements of a list_t 
- *
+ *print_list - print elements of a list_t
+ *@h : head node to the liked list
  *Return: Number of nodes
  *
  */
 size_t print_list(const list_t *h)
 {
-	int counter;
-	char *str = 0;
-	unsigned int len = 0;
+	int counter = 0;
 
 	while (h != NULL)
 	{
@@ -19,11 +17,12 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)");
 		else
 		{
-			printf("%i", len);
-			printf("%s", str);
+			printf("[%i] ", h->len);
+			printf("%s", h->str);
 		}
+		printf("\n");
 		h = h->next;
 		counter++;
 	}
-	return(counter);
+	return (counter);
 }
