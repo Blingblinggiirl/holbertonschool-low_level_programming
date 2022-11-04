5737cb063da1fe11e08fa44a2c9bd632f57f87f6
Blingblinggiirl <Blingblinggiirl@romibenitezz87@gmail.com>
2022-11-04 14:27:13 -0700

    hash

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,10 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
-#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-/**adds new node*/
+#include <stddef.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -21,8 +20,6 @@ typedef struct hash_node_s
 	struct hash_node_s *next;
 } hash_node_t;
 
-/**Hash table */
-
 /**
  * struct hash_table_s - Hash table data structure
  *
@@ -37,6 +34,13 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/* functions */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
 #endif
